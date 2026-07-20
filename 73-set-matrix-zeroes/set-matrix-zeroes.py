266bd700c66d597573d3f7eaf52de7f6264faee3
Vinay_7 <vinaygore115@gmail.com>
2026-07-20 19:08:47 +0530

    Time: 3 ms (91.17%) | Memory: 20.6 MB (15.63%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -3,40 +3,17 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        # m = len(matrix)
-        # n = len(matrix[0])
+        n = len(matrix)
+        m = len(matrix[0])
 
-        # for i in range(m):
-        #     for j in range(n):
-        #         if matrix[i][j] == 0:
-        #             for col in range(n):
-        #                 if matrix[i][col] != 0:
-        #                     matrix[i][col] = -1
-        #             for row in range(m):
-        #                 if matrix[row][j] != 0:
-        #                     matrix[row][j] = -1
-        # for i in range(m):
-        #     for j in range(n):
-        #         if matrix[i][j] == -1:
-        #             matrix[i][j] = 0
-
-        m = len(matrix) # no. of rows
-        n = len(matrix[0]) # no. of columns
-
-        row = [0] * m
-        col = [0] * n
-
-        # 1. mark rows and columns that need to be zeroed
-        for i in range(m):
-            for j in range(n):
-                # If element is zero, mark its row and column
+        row = [0] * n
+        col = [0] * m
+        for i in range(n):
+            for j in range(m):
                 if matrix[i][j] == 0:
                     row[i] = 1
                     col[j] = 1
-
-        # 2. set cells to zero based on markers
-        for i in range(m):
-            for j in range(n):
-                # If the row or column is marked, set cell to zero
-                if row[i] == 1 or col[j] == 1:
+        for i in range(n):
+            for j in range(m):
+                if row[i] or col[j]:
                     matrix[i][j] = 0
