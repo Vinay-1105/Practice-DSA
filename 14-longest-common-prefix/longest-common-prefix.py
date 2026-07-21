1b52e19b44a92495d94ea61f39a4a7ba0415e7d6
Vinay_7 <vinaygore115@gmail.com>
2026-07-22 00:03:24 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (6.47%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -3,27 +3,18 @@ class Solution:
         if not strs:
             return ""
         
-        # Sort the list lexicographically
         strs.sort()
         
-        # First string in sorted list
         first = strs[0]
-        
-        # Last string in sorted list
         last = strs[-1]
-        
-        # Store the common prefix characters
         ans = []
         
-        # Compare characters of first and last string
+        # Comparing characters
         for i in range(min(len(first), len(last))):
-            # Stop if characters differ
             if first[i] != last[i]:
                 return ''.join(ans)
-            # Add matching character to result
             ans.append(first[i])
         
-        # Return the longest common prefix
         return ''.join(ans)       
 
         
