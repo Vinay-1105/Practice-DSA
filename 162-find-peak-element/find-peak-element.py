109fbd0d022cafa1c5033e20591fb5491c42ba4d
Vinay_7 <vinaygore115@gmail.com>
2026-07-30 11:25:39 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (38.33%) - LeetSync

--- 162-find-peak-element/find-peak-element.py
+++ 162-find-peak-element/find-peak-element.py
@@ -4,3 +4,14 @@ class Solution:
         for i in range(n):
             if ((i == 0 or nums[i - 1] < nums[i]) and (i == n - 1 or nums[i] > nums[i + 1])):
                 return i  
+
+        n = len(nums)
+        low = 0
+        high = n - 1
+        while low < high:
+            mid = (low + high) // 2
+            if nums[mid] > nums[high]:
+                high = mid
+            else:
+                low = mid + 1
+        return low 
