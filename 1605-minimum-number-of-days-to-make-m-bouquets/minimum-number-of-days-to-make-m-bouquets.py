e9d0dc0578fc1209e73433b764f407f9ccb71aed
Vinay_7 <vinaygore115@gmail.com>
2026-08-03 11:19:16 +0530

    Time: 312 ms (63.31%) | Memory: 31.5 MB (91.91%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.py
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.py
@@ -1,27 +1,30 @@
 class Solution:
+    def is_possible(self, bloomDay, day, m, k):
+        count = 0
+        bouquets = 0
+        for b in bloomDay:
+            if b <= day:
+                count += 1
+                if count == k:
+                    bouquets += 1
+                    count = 0
+            else:
+                count = 0
+
+        return bouquets >= m
     def minDays(self, bloomDay: List[int], m: int, k: int) -> int:
         if m * k > len(bloomDay):
-            return - 1
-        def possible(day: int) -> bool:
-            count = 0
-            bouquets = 0
-            for i in bloomDay:
-                if i <= day:
-                    count += 1
-                    if count == k:
-                        bouquets += 1
-                        count = 0
-                else:
-                    count = 0
-            return bouquets >= m
-        low, high = min(bloomDay), max(bloomDay)
-        ans = high
+            return -1
+
+        low = min(bloomDay)
+        high = max(bloomDay)
+        res = -1
 
         while low <= high:
             mid = (low + high) // 2
-            if possible(mid):
-                ans = mid
-                high = mid - 1  
+            if self.is_possible(bloomDay, mid, m, k):
+                res = mid
+                high = mid - 1
             else:
                 low = mid + 1
-        return ans
+        return res
