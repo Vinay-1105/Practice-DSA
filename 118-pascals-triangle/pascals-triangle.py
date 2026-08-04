55c6adbf3fa117e95835bbf75be04e959691de2e
Vinay_7 <vinaygore115@gmail.com>
2026-08-05 00:15:38 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (68.11%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.py
+++ 118-pascals-triangle/pascals-triangle.py
@@ -1,6 +1,6 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        triangle = []
+        triangle = [] # For storing the rows
         for i in range(numRows):
             row = [1] * (i + 1)
             for j in range(1, i):
