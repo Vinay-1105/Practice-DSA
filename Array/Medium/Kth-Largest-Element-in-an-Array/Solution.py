a9ebc626f45a688bc30d147b7bc43d22f8273614
Vinay_7 <vinaygore115@gmail.com>
2026-08-16 16:30:24 +0530

    Time: 106 ms (34.10%) | Memory: 31 MB (47.88%) - GetLeet

--- Array/Medium/Kth-Largest-Element-in-an-Array/Solution.py
+++ Array/Medium/Kth-Largest-Element-in-an-Array/Solution.py
@@ -1,4 +1,11 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        nums.sort()
-        return nums[-k]
+        # nums.sort()
+        # return nums[-k]
+
+        heap = []
+        for n in nums:
+            heapq.heappush(heap, n)
+            if len(heap) > k:
+                heapq.heappop(heap)
+        return heap[0]
