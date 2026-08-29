9da839b969195325858a72f9d5fbaa859ab7d8b1
Vinay_7 <vinaygore115@gmail.com>
2026-08-29 23:07:20 +0530

    Time: 0 ms (100.00%) | Memory: 20.5 MB (36.83%) - GetLeet

--- Array/Easy/Binary-Search/Solution.py
+++ Array/Easy/Binary-Search/Solution.py
@@ -2,7 +2,7 @@ class Solution:
     def search(self, nums: List[int], target: int) -> int:
         n = len(nums)
         low = 0
-        high = n - 1
+        high = n-1
         while low <= high:
             mid = (low + high) // 2
             if nums[mid] == target:
