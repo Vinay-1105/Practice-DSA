36447c20beba52742fa3807795939af144e2ab84
Vinay_7 <vinaygore115@gmail.com>
2026-08-30 22:59:18 +0530

    Time: 48 ms (54.29%) | Memory: 19.7 MB (5.00%) - GetLeet

--- Linked-List/Medium/Delete-Node-in-a-Linked-List/Solution.py
+++ Linked-List/Medium/Delete-Node-in-a-Linked-List/Solution.py
@@ -10,5 +10,5 @@ class Solution:
         :type node: ListNode
         :rtype: void Do not return anything, modify node in-place instead.
         """ 
-        node.val = node.next.val
-        node.next = node.next.next
+        node.val = node.next.val#Copying the next nodes val
+        node.next = node.next.next#Skip the curr node, and delete it
