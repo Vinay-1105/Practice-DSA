23f7a50d407a73f7b7ede135e3854c30f152acf8
Vinay_7 <vinaygore115@gmail.com>
2026-09-03 22:12:59 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (62.78%) - GetLeet

--- Array/Medium/Sort-Colors/Solution.py
+++ Array/Medium/Sort-Colors/Solution.py
@@ -3,18 +3,28 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # We can solve this question using a special algorithm called
-        # Dutch National Flag Algorithm
-        low = 0
-        mid = 0
-        high = len(nums) - 1
-        while mid <= high:
-            if nums[mid] == 0:
-                nums[low], nums[mid] = nums[mid], nums[low]
-                low += 1
-                mid += 1
-            elif nums[mid] == 1:
-                mid += 1
+        # Brute force approach
+        # First we will check how 0's, 1's and 2's are present in the array 
+        # And based on that we will fill the array first with 0's, then 1's and lastly 2's 
+        # basically overwriting in it
+        cnt0 = 0
+        cnt1 = 0
+        cnt2 = 0
+        for num in nums:
+            if num == 0:
+                cnt0 += 1
+            elif num == 1:
+                cnt1 += 1
             else:
-                nums[mid], nums[high] = nums[high], nums[mid]
-                high -= 1 
+                cnt2 += 1
+        
+        idx = 0
+        for _ in range(cnt0):
+            nums[idx] = 0
+            idx += 1
+        for _ in range(cnt1):
+            nums[idx] = 1
+            idx += 1
+        for _ in range(cnt2):
+            nums[idx] = 2
+            idx += 1
