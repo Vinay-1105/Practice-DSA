71d19b78fb04da93c30590dc001c05344966863e
Vinay_7 <vinaygore115@gmail.com>
2026-09-04 16:16:11 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (89.46%) - GetLeet

--- Linked-List/Easy/Middle-of-the-Linked-List/Solution.py
+++ Linked-List/Easy/Middle-of-the-Linked-List/Solution.py
@@ -5,9 +5,16 @@
 #         self.next = next
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        slow_ptr = head
-        fast_ptr = head
-        while fast_ptr and fast_ptr.next:
-            slow_ptr = slow_ptr.next
-            fast_ptr = fast_ptr.next.next
-        return slow_ptr
+        temp = head
+        cnt = 0
+        while temp:
+            cnt += 1
+            temp = temp.next
+        mid = (cnt//2)+1
+        temp = head
+        while temp:
+            mid = mid-1
+            if mid == 0:
+                break
+            temp = temp.next
+        return temp
