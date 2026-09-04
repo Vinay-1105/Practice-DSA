d7e1f8b4f06af6479e533f838fecc81997f26c69
Vinay_7 <vinaygore115@gmail.com>
2026-09-04 16:13:17 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (20.06%) - GetLeet

--- Linked-List/Easy/Middle-of-the-Linked-List/Solution.py
+++ Linked-List/Easy/Middle-of-the-Linked-List/Solution.py
@@ -7,7 +7,7 @@ class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
         slow_ptr = head
         fast_ptr = head
-        while fast_ptr is not None and fast_ptr.next is not None:
+        while fast_ptr and fast_ptr.next:
             slow_ptr = slow_ptr.next
             fast_ptr = fast_ptr.next.next
         return slow_ptr
