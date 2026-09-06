c5230122482b6cff3008d925cb33a3cf225f7c92
Vinay_7 <vinaygore115@gmail.com>
2026-09-06 16:46:52 +0530

    Time: 55 ms (43.20%) | Memory: 22.4 MB (21.76%) - GetLeet

--- Hash-Table/Medium/Linked-List-Cycle-II/Solution.py
+++ Hash-Table/Medium/Linked-List-Cycle-II/Solution.py
@@ -6,11 +6,16 @@
 
 class Solution:
     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        temp = head
-        seen = set()
-        while temp:
-            if temp in seen:
-                return temp
-            seen.add(temp)
-            temp = temp.next
+        # Hum iss question ko Tortoise And Hare Algorithm se solve kar sakte hai
+        slow = head
+        fast = head
+        while fast and fast.next:
+            slow = slow.next # Slow pointer moves one place at a time
+            fast = fast.next.next # Whereas, fast pointer moves 2 places at a time
+            if slow == fast: # hum yaha par check karte hai ki slow & fast have collided
+                slow = head
+                while slow != fast:
+                    slow = slow.next
+                    fast = fast.next
+                return fast
         return None
