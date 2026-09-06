e573bac719a87ac1acf104140db83fb59931cefd
Vinay_7 <vinaygore115@gmail.com>
2026-09-06 16:18:25 +0530

    Time: 56 ms (36.57%) | Memory: 22.6 MB (10.53%) - GetLeet

---
+++ Hash-Table/Medium/Linked-List-Cycle-II/Solution.py
@@ -0,0 +1,15 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        temp = head
+        seen = set()
+        while temp:
+            if temp in seen:
+                return temp
+            seen.add(temp)
+            temp = temp.next
