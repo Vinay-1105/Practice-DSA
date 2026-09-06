39b3655b513b05d7bb2d9dd46e1e1fcde58c2b7f
Vinay_7 <vinaygore115@gmail.com>
2026-09-06 15:51:43 +0530

    Time: 51 ms (72.10%) | Memory: 22.5 MB (54.81%) - GetLeet

--- Hash-Table/Easy/Linked-List-Cycle/Solution.py
+++ Hash-Table/Easy/Linked-List-Cycle/Solution.py
@@ -6,11 +6,11 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        slow_pointer = head # Moves one node at a time
-        fast_pointer = head # Moves two nodes at a time
-        while fast_pointer and fast_pointer.next:
-            slow_pointer = slow_pointer.next
-            fast_pointer = fast_pointer.next.next
-            if slow_pointer == fast_pointer: # It basically checks if there is a cycle/loop that exists or not
+        temp = head
+        nodeHash = {}
+        while temp:
+            if temp in nodeHash:
                 return True
+            nodeHash[temp] = 1
+            temp = temp.next
         return False
