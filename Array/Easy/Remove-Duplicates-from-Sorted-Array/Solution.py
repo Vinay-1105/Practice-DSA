fa98a8d3e6776bb8fbffe640f36e299ea5c4716e
Vinay_7 <vinaygore115@gmail.com>
2026-09-13 22:21:21 +0530

    Time: 0 ms (100.00%) | Memory: 20.5 MB (42.92%) - GetLeet

--- Array/Easy/Remove-Duplicates-from-Sorted-Array/Solution.py
+++ Array/Easy/Remove-Duplicates-from-Sorted-Array/Solution.py
@@ -1,11 +1,21 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        seen = set()
-        cnt = 0
+        # seen = set()
+        # cnt = 0
+        # n = len(nums)
+        # for i in range(n):
+        #     if nums[i] not in seen:
+        #         seen.add(nums[i])
+        #         nums[cnt] = nums[i]
+        #         cnt += 1
+        # return cnt
+
+        if not nums:
+            return 0
         n = len(nums)
-        for i in range(n):
-            if nums[i] not in seen:
-                seen.add(nums[i])
-                nums[cnt] = nums[i]
-                cnt += 1
-        return cnt
+        i = 0
+        for j in range(1, n):
+            if nums[i] != nums[j]:
+               i += 1
+               nums[i] = nums[j]
+        return i+1
