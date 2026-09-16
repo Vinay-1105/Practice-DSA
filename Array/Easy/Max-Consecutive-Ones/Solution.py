61a1579dabadd9d56ec25bca4e61a8cf38dc5599
Vinay_7 <vinaygore115@gmail.com>
2026-09-16 10:23:56 +0530

    Time: 21 ms (27.23%) | Memory: 22 MB (5.25%) - GetLeet

---
+++ Array/Easy/Max-Consecutive-Ones/Solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: list[int]) -> int:
+        n = len(nums)
+        maxi=0
+        cnt=0
+        for i in range(n):
+            if nums[i] == 1:
+                cnt+=1
+                maxi=max(maxi, cnt)
+            else:
+                cnt=0
+        return maxi
