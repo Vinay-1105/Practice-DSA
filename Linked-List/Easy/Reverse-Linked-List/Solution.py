ffcc2f86e30f1808168b5538f780896314ab8da7
Vinay_7 <vinaygore115@gmail.com>
2026-09-20 19:12:49 +0530

    Time: 0 ms (100.00%) | Memory: 20.7 MB (7.60%) - GetLeet

--- Linked-List/Easy/Reverse-Linked-List/Solution.py
+++ Linked-List/Easy/Reverse-Linked-List/Solution.py
@@ -4,12 +4,12 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        prev = None
-        curr = head
+    def reverseList(self, head: ListNode | None) -> ListNode | None:
+        prev=None
+        curr=head
         while curr:
-            next_node = curr.next
-            curr.next = prev
-            prev = curr
-            curr = next_node
+            new_node=curr.next
+            curr.next=prev
+            prev=curr
+            curr=new_node
         return prev
