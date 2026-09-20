bfce7d51b33c2b208260636316c1d37cb65dd272
Vinay_7 <vinaygore115@gmail.com>
2026-09-20 18:58:43 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (25.92%) - GetLeet

--- Linked-List/Medium/Remove-Nth-Node-From-End-of-List/Solution.py
+++ Linked-List/Medium/Remove-Nth-Node-From-End-of-List/Solution.py
@@ -5,19 +5,30 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: ListNode | None, n: int) -> ListNode | None:
-        cnt=0
-        temp=head
-        while temp:
-            cnt+=1
-            temp=temp.next
-        if cnt == n:
+        # cnt=0
+        # temp=head
+        # while temp:
+        #     cnt+=1
+        #     temp=temp.next
+        # if cnt == n:
+        #     return head.next
+        # res=cnt-n
+        # temp=head
+        # while temp:
+        #     res-=1
+        #     if res==0:
+        #         break
+        #     temp=temp.next
+        # temp.next=temp.next.next
+        # return head
+        fast = head
+        for _ in range(n):
+            fast=fast.next
+        if fast is None:
             return head.next
-        res=cnt-n
-        temp=head
-        while temp:
-            res-=1
-            if res==0:
-                break
-            temp=temp.next
-        temp.next=temp.next.next
+        slow=head
+        while fast.next:
+            slow=slow.next
+            fast=fast.next
+        slow.next=slow.next.next
         return head
