5f4ac991846b18b0dcade79989d48bd565114f87
Vinay_7 <vinaygore115@gmail.com>
2026-09-22 19:12:54 +0530

    Time: 110 ms (67.65%) | Memory: 38.3 MB (15.21%) - GetLeet

--- Hash-Table/Easy/Intersection-of-Two-Linked-Lists/Solution.py
+++ Hash-Table/Easy/Intersection-of-Two-Linked-Lists/Solution.py
@@ -6,15 +6,37 @@
 
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
-        nodeHash={}
-        temp=headA
-        while temp:
-            nodeHash[temp]=1
-            temp=temp.next
+        temp1=headA
+        temp2=headB
+        len1=0
+        len2=0
+
+        while temp1:
+            len1 += 1
+            temp1 = temp1.next
+        while temp2:
+            len2 += 1
+            temp2 = temp2.next
         
-        temp=headB
-        while temp:
-            if temp in nodeHash:
-                return temp
-            temp=temp.next
+        #reset the pointers again
+        temp1=headA
+        temp2=headB
+        skip1=max(0, len1-len2)
+        skip2=max(0, len2-len1)
+
+        # Skipping the starting nodes if any
+        while skip1>0:
+            temp1 = temp1.next
+            skip1 -= 1
+        while skip2>0:
+            temp2 = temp2.next
+            skip2 -= 1
+        
+        #Here, both the pointers are at the same distance from the end
+        #Hum abh sath mai inko move karega jab hume intersection nahi milta
+        while temp1 and temp2:
+            if temp1==temp2:
+                return temp1
+            temp1=temp1.next
+            temp2=temp2.next
         return None
